c81e092572c4b8539bc5d75ddf5d04001f16962e
Rokas Kirdulis <rokas.kirdulis@gmail.com>
2025-09-18 19:55:55 +0200

    Images fixed

--- app/components/landing/Hero.vue
+++ app/components/landing/Hero.vue
@@ -36,8 +36,8 @@ defineProps<{
       >
         <UColorModeAvatar
           class="size-18 ring ring-default ring-offset-3 ring-offset-(--ui-bg)"
-          light="https://res.cloudinary.com/donpcwlwk/image/upload/v1714638525/IMG_8227_gy6ylo_Squared_q8szk6.jpg"
-          dark="https://res.cloudinary.com/donpcwlwk/image/upload/v1714638525/IMG_8227_gy6ylo_Squared_q8szk6.jpg"
+          light="/img/avatar.jpg"
+          dark="/img/avatar.jpg"
           :alt="global.picture?.alt!"
         />
       </Motion>
